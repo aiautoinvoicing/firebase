180f225287251dccccb01f0ba61e24d8e832553b
aiaccfin <aiaccfin@gmail.com>
2025-07-03 16:23:46 -0400

    update

--- src/constants/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from '@/src/constants/colors';
-export * from '@/src/constants/s_global';
-export * from '@/src/constants/s_modal';
-export * from '@/src/constants/s_fab';
-export * from '@/src/constants/s_inv';
--- src/stores/index.tsx
+++
@@ -1,4 +0,0 @@
-export * from '@/src/stores/useClientStore';
-export * from '@/src/stores/useModalStore';
-export * from '@/src/stores/useItemStore';
-export * from '@/src/stores/useGlobalStore';
